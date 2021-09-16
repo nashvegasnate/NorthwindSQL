94c32708bb99f7cc827e77552f7646153dbfc8d0
Nathan Keith <nashvegasnate@gmail.com>
2021-09-16 10:03:14 -0500

    completed query

--- a. undiscounted_subtotals.sql
+++ a. undiscounted_subtotals.sql
@@ -1,2 +1,12 @@
 --1. What is the undiscounted subtotal for each Order 
 --(identified by OrderID).
+
+select [Order Details].OrderID,
+  SUM(Quantity * UnitPrice) AS 'Undiscounted Subtotal'
+FROM [Order Details]
+  JOIN Orders
+    ON Orders.OrderId = [Order Details].OrderId
+GROUP BY [Order Details].OrderId
+
+--To group in descending order of total, add this:
+--order by 'Undiscounted Subtotal' desc
